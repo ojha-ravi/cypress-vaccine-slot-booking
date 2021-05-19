f9b24e736d18dc0fc1e4d8def2c3a86e5c56feb2
ymishra-se <68019301+ymishra-se@users.noreply.github.com>
2021-05-19 12:50:53 +0530

    Update slot_booking_bbmp.js
    Added rerun slot search until slot available. Will show popup if slots available. Need to add some delay in rerun and desktop notification on success.

--- cypress/integration/slot_booking_bbmp.js
+++ cypress/integration/slot_booking_bbmp.js
@@ -2,45 +2,46 @@ describe("My First Test", () => {
   it("Open Cowin App!", () => {
     cy.viewport(1000, 1000);
     cy.visit("https://selfregistration.cowin.gov.in/");
-    cy.get("input#mat-input-0").type("<your_mobile_number>");
+    cy.get("input#mat-input-0").type("9968726089");
     cy.get(".next-btn").click();
     cy.get("input#mat-input-1").focus();
-    cy.pause();
-
-    cy.get(".next-btn").click();
-
-    cy.intercept("*beneficiaries*").as("beneficiaries");
-    cy.wait(["@beneficiaries"]);
-
-    cy.get(".btnlist > .bordernone").first().click();
-
-    // TO Test
-    // cy.visit("https://selfregistration.cowin.gov.in/appointment");
-    cy.get(".custom-checkbox").click();
-
-    cy.get(".mat-form-field-infix").first().click();
-    cy.get("#mat-option-16").click();
-
-    cy.wait(100);
-
-    // cy.intercept("*districts*").as("districts");
-    // cy.wait(["@districts"]);
-
-    cy.get(".mat-form-field-infix").last().click();
-    cy.get("#mat-option-40").click();
-
-    cy.get(".pin-search-btn").click();
-
-    // cy.intercept("*calendarByDistrict*").as("calendarByDistrict");
-    // cy.wait(["@calendarByDistrict"]);
-
-    cy.get(".agefilterblock .form-check").first().click();
-
-    const slot = cy.get(".slots-box").not(".no-seat").not(".no-available");
-    if (slot) {
-      slot.first().click();
-    } else {
-      console.log("Not Available");
-    }
+    cy.pause();    
   });
+  /*
+  * 1. User needs to click on Verify button 
+  * 2. User needs to select beneficiaries by clicking on schedule 
+  * 3. Click Cypress Resume
+  * 4. Below method will rerun untill it will find a slot 
+  * 5. Need to add some delay 
+  */
+  it('.select() - select an option in a <select> element', 
+  {
+    "retries": 200000
+  },() => {    
+    // https://on.cypress.io/select   
+    
+    cy.get('.custom-checkbox').click('right')
+    cy.get('[formcontrolname="state_id"]').click()
+    cy.wait(100)
+    cy.get('[role="listbox"]').contains('Karnataka').click()
+    cy.wait(500)
+    cy.get('[formcontrolname="district_id"]').click()
+    cy.wait(100)
+    cy.get('[role="listbox"]').contains('BBMP').click()   
+    cy.wait(500)
+    cy.get('.district-search').click()
+    cy.wait(1000)
+    cy.get(".agefilterblock .form-check").first().click({force: true});
+    cy.wait(500)  
+    cy.get(".slots-box").not(".no-seat").not(".no-available")
+    .should(($div) => {
+      const n = parseFloat($div.text())
+      expect(n).to.be.gte(0)
+      if (n >= 1) {
+          // you can throw your own errors
+          alert('Vaccine Slot Available')
+        }
+    })
+    
+  })  
 });
